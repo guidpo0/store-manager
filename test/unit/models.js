2c3473557f0690bbb41e730ef9171da6e2451eb1
Gui Pereira <guilhermepdo1508@gmail.com>
2021-10-18 15:43:32 -0300

    Feito teste de model para req 1

---
+++ test/unit/models.js
@@ -0,0 +1,55 @@
+const { expect } = require('chai');
+const { MongoClient } = require('mongodb');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const sinon = require('sinon');
+const mongoConnection = require('../../models/connection');
+
+const ProductsModel = {
+  create: () => null,
+};
+
+let connectionMock;
+const mongoConnectionStub = async () => {
+  const DB_NAME = 'StoreManager';
+  const DBServer = new MongoMemoryServer();
+  const URLMock = await DBServer.getUri();
+  connectionMock = await MongoClient
+    .connect(URLMock, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    .then((conn) => conn.db(DB_NAME));      
+  sinon.stub(mongoConnection, 'getConnection').resolves(connectionMock);
+};
+
+describe('1 - Model - Insere um novo produto no BD', () => {
+  const payloadProduct = {
+    name: 'Example Product',
+    quantity: 2000,
+  }
+  describe('quando é inserido com sucesso', () => {
+    before(mongoConnectionStub);
+
+    after(() => {
+      mongoConnection.getConnection.restore();
+    });
+
+    it('retorna um objeto', async () => {
+      const response = await ProductsModel.create(payloadProduct);
+      expect(response).to.be.a('object')
+    });
+
+    it('tal objeto possui o "id" do novo produto inserido', async () => {
+      const response = await ProductsModel.create(payloadProduct);
+      expect(response).to.have.a.property('id')
+    });
+
+    it('deve existir um produto com o nome cadastrado!', async () => {
+      await MoviesModel.create(payloadProduct);
+      const productCreated = await connectionMock.collection(
+        'products',
+      ).findOne({ name: productCreated.name });
+      expect(productCreated).to.be.not.null;
+    });
+  });
+});
