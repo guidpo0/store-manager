a42364dd0505064d79f4c0feb5fe61c3aec808b2
Gui Pereira <guilhermepdo1508@gmail.com>
2021-10-21 12:03:10 -0300

    Feito testes controller req 5

---
+++ controllers/ProductsErrorController.js
@@ -0,0 +1,16 @@
+const { UNPROCESSABLE_ENTITY, INTERNAL_SERVER_ERROR } = require('../helpers/HTTPCodes');
+
+const ProductsErrorController = (err, _req, res, _next) => {
+  if (err.isJoi) {
+    const { message } = err.details[0]; 
+    const error = { code: 'invalid_data', message };
+    return res.status(UNPROCESSABLE_ENTITY).json({ err: error });
+  }
+  if (err.code === 'invalid_data') {
+    return res.status(UNPROCESSABLE_ENTITY).json({ err });
+  }
+  const serverError = { code: 'internal_error', message: 'Erro de servidor' };
+  res.status(INTERNAL_SERVER_ERROR).json({ err: serverError });
+};
+
+module.exports = ProductsErrorController;
