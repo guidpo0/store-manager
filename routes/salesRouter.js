4aef523c8d09972f14c76964c7327f98e6d575b0
Gui Pereira <guilhermepdo1508@gmail.com>
2021-10-21 15:15:54 -0300

    Feito req 8

--- routes/salesRouter.js
+++ routes/salesRouter.js
@@ -9,7 +9,7 @@ router.post('/', rescue(SalesController.create));
 router.get('/', rescue(SalesController.getAll));
 router.get('/:id', rescue(SalesController.getById));
 router.put('/:id', rescue(SalesController.update));
-// router.delete('/:id', rescue(SalesController.remove));
+router.delete('/:id', rescue(SalesController.remove));
 
 router.use(SalesErrorController);
 
