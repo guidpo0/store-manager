acded3069b7ef15e8eaadae6b4d0f2d3fb052f9a
Gui Pereira <guilhermepdo1508@gmail.com>
2021-10-21 13:05:41 -0300

    Feito req 6

--- routes/salesRouter.js
+++ routes/salesRouter.js
@@ -6,8 +6,8 @@ const SalesErrorController = require('../controllers/SalesErrorController');
 const router = express.Router();
 
 router.post('/', rescue(SalesController.create));
-// router.get('/', rescue(SalesController.getAll));
-// router.get('/:id', rescue(SalesController.getById));
+router.get('/', rescue(SalesController.getAll));
+router.get('/:id', rescue(SalesController.getById));
 // router.put('/:id', rescue(SalesController.update));
 // router.delete('/:id', rescue(SalesController.remove));
 
